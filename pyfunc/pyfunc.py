3510509f191df1237c015014ccd51103dda8c77b
daniel-davidd <daniel@ip-192-168-1-138.eu-central-1.compute.internal>
2023-10-01 14:54:36 +0300

    added requirements file

--- pyfunc/pyfunc.py
+++ pyfunc/pyfunc.py
@@ -1,10 +1,11 @@
 import logging
+import mmh3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    message = 'Hello {} #### !'.format(event['data'])  
+    msgmmh = mmh3.hash(event['data'])
     return { 
-        'message' : message
+        'message' : msgmmh
     }
