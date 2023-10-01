74f3a1a7a7bd602f2e4f018456d1a66087a2dd07
daniel-davidd <daniel@ip-192-168-1-138.eu-central-1.compute.internal>
2023-10-01 12:59:21 +0300

    added py function

--- pyfunc/pyfunction.py
+++ pyfunc/pyfunction.py
@@ -4,8 +4,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    logger.info('Event: {}'.format(event))
-    logger.info('Context: {}'.format(context))
-    result = event['data'] + "heiiiiiiii"
-    response = {'data': result}
-    return response
+    message = 'Hello {} #### !'.format(event['data'])  
+    return { 
+        'message' : message
+    }
