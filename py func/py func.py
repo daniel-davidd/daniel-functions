1356de91a48dc442369f898d7268aeca7beb065d
daniel-davidd <daniel@ip-192-168-1-106.eu-central-1.compute.internal>
2023-11-21 18:01:11 +0200

    edit the python function

--- py func/py func.py
+++ py func/py func.py
@@ -1,13 +1,13 @@
-import logging
-import jsonschema
+import json
+import base64
+from memphis.functions import create_function
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+def handler(event, context): 
+    return create_function(event, event_handler = event_handler)
 
-def lambda_handler(event, context):
-    schema = {
-        'data' : {'type' : 'string'}
-    }
-    return { 
-        'message' : jsonschema.validate(event, schema)
-    }
+def event_handler(msg_payload, msg_headers, inputs):
+    payload =  str(msg_payload, 'utf-8')
+    as_json = json.loads(payload)
+    as_json['went in lambda'] = "yessir"
+
+    return bytes(json.dumps(as_json), encoding='utf-8'), msg_headers
