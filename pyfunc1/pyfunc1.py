03640b0bc130ad93fb48a888a525539a650da7a5
daniel-davidd <daniel@ip-192-168-1-106.eu-central-1.compute.internal>
2023-11-21 18:37:31 +0200

    a

--- pyfunc1/pyfunc1.py
+++ pyfunc1/pyfunc1.py
@@ -9,6 +9,6 @@ def event_handler(msg_payload, msg_headers, inputs):
     payload =  str(msg_payload, 'utf-8')
     as_json = json.loads(payload) 
     as_json['went in lambda'] = "yessir"
-    
+    as_json['went in lambda2'] = "yessir2"
 
     return bytes(json.dumps(as_json), encoding='utf-8'), msg_headers
